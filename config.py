9d45d009bd5231ede2dd2180a166238e106f6573
Samarth Gupta <samarthgupta@Samarths-MacBook-Pro.local>
2023-07-28 08:48:16 -0400

    exception handling added  to GetAllBooks

--- config.py
+++ config.py
@@ -6,4 +6,12 @@ db = client['library_db']
 books_collection = db['books']
 
 def get_books():
-    return list(books_collection.find())
+    try:
+        return list(books_collection.find())
+    except Exception as e:
+        df = {
+            "Error_Message": "Something went wrong in the get_books method",
+            "Error" : e.args[0]
+        }
+        raise e
+        return df
