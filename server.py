191479ce2115c7fc10ec24546a175d7ad60f46e1
Samarth Gupta <samarthgupta@Samarths-MacBook-Pro.local>
2023-07-31 01:20:55 -0400

    createBook

--- server.py
+++ server.py
@@ -16,3 +16,14 @@ def get_all_books():
         raise e
         return df
 
+@app.post("/books/", response_model=Book)
+def create_new_book(book: Book):
+    try:
+        return create_book(book)
+    except Exception as e:
+        df = {
+            "Error_Message": "Something went wrong in the create_new_book method",
+            "Error" : e.args[0]
+        }
+        raise e
+        return df  
